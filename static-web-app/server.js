f896fba57b15dfdb19dcffb15cb9c179c1bbb1f3
Your Name <me@mail.com>
2018-08-24 13:36:23 +0800

    Subtree change

--- static-web-app/server.js
+++ static-web-app/server.js
@@ -7,6 +7,7 @@ server.get('/*', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
+// Another change via subtree
 var port = process.env.PORT || 3000
 server.listen(port, function () {
   console.log('server listening on port ' + port)
