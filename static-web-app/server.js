39c791085f70b295858c55f65b48f38239041bab
Your Name <me@mail.com>
2018-08-24 12:20:06 +0800

    Static web app

---
+++ static-web-app/server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+
+var server = express()
+server.use('/', express.static(__dirname + '/'))
+
+server.get('/*', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+var port = process.env.PORT || 3000
+server.listen(port, function () {
+  console.log('server listening on port ' + port)
+})
