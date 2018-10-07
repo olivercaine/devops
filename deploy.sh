910b152d61b93a26a60fc6b0b81442bd90ff0f74
Oliver Caine <olliecaine@gmail.com>
2018-10-07 12:25:03 +1100

    Creating build_and_push_container function

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Exit immediately if a simple command exits with a nonzero exit value
+# Exit immediately if a simple command exits with a non-zero exit value
 set -e
 
 repo_url=$(git config --get remote.origin.url)
@@ -25,7 +25,7 @@ echo "BITBUCKET_COMMIT $BITBUCKET_COMMIT"
 HEROKU_APP_NAME="$PROJECT_NAME-$BITBUCKET_BRANCH"
 echo "HEROKU_APP_NAME $HEROKU_APP_NAME"
 
-install_lint_and_test () {
+function install_lint_and_test {
     echo "cd to $1..."
     cd ./$1
 
@@ -38,18 +38,13 @@ install_lint_and_test () {
     cd ../ 
 }
 
-deploy_client () {
-    echo "cd to directory..."
-    cd ./client
+function build_and_push_container {
+    HEROKU_APP_NAME=$1
+    BITBUCKET_COMMIT=$2
+    HEROKU_API_KEY=$3
 
     echo "Creating app '$HEROKU_APP_NAME'..."
     heroku create -a $HEROKU_APP_NAME --region eu || true
-
-    echo "Creating dist..."
-    npm run build
-
-    echo "Copying in Node app..."
-    cp ../devops/static-web-app/* ./dist
 
     echo "Building image '$HEROKU_APP_NAME:$BITBUCKET_COMMIT' and pushing to Heroku..."
     docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
@@ -64,41 +59,33 @@ deploy_client () {
     echo "Releasing container to '$HEROKU_APP_NAME'..."
     heroku container:release web -a $HEROKU_APP_NAME
 
-    echo "Deployed app to https://$HEROKU_APP_NAME-s.herokuapp.com"
+    echo "Deployed app to https://$HEROKU_APP_NAME.herokuapp.com"
+}
+
+function deploy_client {
+    echo "cd to directory..."
+    cd ./client
+
+    echo "Creating dist..."
+    npm run build
+
+    echo "Copying in Node app..."
+    cp ../devops/static-web-app/* ./dist
+
+    build_and_push_container $HEROKU_APP_NAME $BITBUCKET_COMMIT $HEROKU_API_KEY
 
     echo "cd to root..."
     cd ../
 }
 
-deploy_server () {
+function deploy_server {
     echo "cd to directory..."
     cd ./server
 
-    echo "Creating app '$HEROKU_APP_NAME-s'..."
-    heroku create -a $HEROKU_APP_NAME-s --region eu || true
-
-    echo "Building image '$HEROKU_APP_NAME-s:$BITBUCKET_COMMIT' and pushing to Heroku..."
-    docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-
-    echo "Prepping env file..."
-    cp -R .env.production .env
-    sed -i.bak "s/^\(APP_NAME=\).*/\1'$BITBUCKET_BRANCH #$BITBUCKET_COMMIT'/" .env
-    cat .env
-
-    echo "Building and pushing container..."
-    ID=$(docker build . -q -t $HEROKU_APP_NAME-s:$BITBUCKET_COMMIT)
-    NAME="registry.heroku.com/$HEROKU_APP_NAME-s/web"
-    docker tag $ID $NAME
-    docker images
-    docker push $NAME
-    
-    echo "Releasing container to '$HEROKU_APP_NAME-s'..."
-    heroku container:release web -a $HEROKU_APP_NAME-s
-
-    echo "Deployed app to https://$HEROKU_APP_NAME-s.herokuapp.com"
+    build_and_push_container "$HEROKU_APP_NAME-s" $BITBUCKET_COMMIT $HEROKU_API_KEY
 
     echo "cd to root..."
-    cd ../../
+    cd ../
 }
 
 if [ -d module ]; then
@@ -106,5 +93,6 @@ if [ -d module ]; then
 fi
 install_lint_and_test client
 install_lint_and_test server
+
 deploy_client
 deploy_server
