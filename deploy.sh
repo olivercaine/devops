92f6bc9def432ddfdf29d701f57dc22e44451871
Oliver Caine <olliecaine@gmail.com>
2018-11-20 00:25:49 +1100

    Parallelising docker build and push

--- deploy.sh
+++ deploy.sh
@@ -37,34 +37,30 @@ trim_string () {
     echo $string
 }
 
-build_container () {
-    local heroku_app_name=$1
-
-    echo "Creating app '$heroku_app_name'..."
-    heroku create -a $heroku_app_name --region eu || true
-
-    echo "Building image '$heroku_app_name:$BITBUCKET_COMMIT' and pushing to Heroku..."
-    docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-
-    echo "Building and pushing container..."
-    ID=$(docker build . -q -t $heroku_app_name:$BITBUCKET_COMMIT)
-    NAME="registry.heroku.com/$heroku_app_name/web"
-    docker tag $ID $NAME
-    docker push $NAME
-    
-    echo "Releasing container to '$heroku_app_name'..."
-    heroku container:release web -a $heroku_app_name
-
-    echo "Deployed app to https://$heroku_app_name.herokuapp.com"
-}
-
-deploy () {
+docker_build_and_push () {
     if [ -d $1 ]; then
         echo "cd to $1..."
         cd ./$1
 
         suffix="$(echo $1 | head -c 1)"
-        build_container $HEROKU_APP_NAME-$suffix
+        local heroku_app_name=$HEROKU_APP_NAME-$suffix
+
+        echo "Creating app '$heroku_app_name'..."
+        heroku create -a $heroku_app_name --region eu || true
+
+        echo "Building image '$heroku_app_name:$BITBUCKET_COMMIT' and pushing to Heroku..."
+        docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+
+        echo "Building and pushing container..."
+        ID=$(docker build . -q -t $heroku_app_name:$BITBUCKET_COMMIT)
+        NAME="registry.heroku.com/$heroku_app_name/web"
+        docker tag $ID $NAME
+        docker push $NAME
+        
+        echo "Releasing container to '$heroku_app_name'..."
+        heroku container:release web -a $heroku_app_name
+
+        echo "Deployed app to https://$heroku_app_name.herokuapp.com"
 
         echo "cd to root..."
         cd ../
@@ -84,7 +80,7 @@ install_lint_test_and_build () {
 
         echo "Creating dist..."
         npm run build
-        
+
         echo "cd to root..."
         cd ../ 
     fi
@@ -96,5 +92,5 @@ install_lint_test_and_build module
 install_lint_test_and_build client
 install_lint_test_and_build server
 
-deploy client
-deploy server
+docker_build_and_push client &
+docker_build_and_push server
