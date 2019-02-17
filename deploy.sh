7c901946b34d28c009d8d6cceeabee30467f5ecf
Oliver Caine <olliecaine@gmail.com>
2019-02-17 12:14:50 +1100

    Only installing Heroku if it's not already installed

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e # Exit on non-zero exit value
 
---
+++ install-heroku-cli.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# To uninstall:
+# rm -rf /usr/local/heroku /usr/local/lib/heroku /usr/local/bin/heroku ~/.local/share/heroku ~/Library/Caches/heroku
+
+if [ "$(which heroku)" != '/usr/local/bin/heroku' ]; then
+    echo "Heroku not installed. Installing..."
+    curl https://cli-assets.heroku.com/install.sh | sh; heroku --version; which heroku;
+else
+    echo "Heroku already installed!"
+fi
