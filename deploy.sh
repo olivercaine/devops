068b6ea9b1115f686969751da52619df45766b28
Oliver Caine <olliecaine@gmail.com>
2019-02-17 20:23:53 +1100

    Reorganising deplou

--- deploy.sh
+++ deploy.sh
@@ -37,36 +37,6 @@ prep_heroku_app_name () {
     echo $string | tr / - # Heroky doesn't allow "/" so replace it with "-"
 }
 
-docker_build_and_push () {
-    if [ -d $1 ]; then
-        echo "cd to $1..."
-        cd ./$1
-
-        suffix="$(echo $1 | head -c 1)"
-        local heroku_app_name=$HEROKU_APP_NAME-$suffix
-
-        echo "Creating app '$heroku_app_name'..."
-        heroku create -a $heroku_app_name --region eu || true
-
-        echo "Building image '$heroku_app_name:$BITBUCKET_COMMIT' and pushing to Heroku..."
-        docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-
-        echo "Building and pushing container..."
-        ID=$(docker build . -q -t $heroku_app_name:$BITBUCKET_COMMIT)
-        NAME="registry.heroku.com/$heroku_app_name/web"
-        docker tag $ID $NAME
-        docker push $NAME
-        
-        echo "Releasing container to '$heroku_app_name'..."
-        heroku container:release web -a $heroku_app_name
-
-        echo "Deployed app to https://$heroku_app_name.herokuapp.com"
-
-        echo "cd to root..."
-        cd ../
-    fi
-}
-
 install_lint_test_and_build () {
     local directory=$1
     local use_docker=$2
@@ -97,10 +67,40 @@ install_lint_test_and_build () {
     fi
 }
 
+docker_build_and_push () {
+    if [ -d $1 ]; then
+        echo "cd to $1..."
+        cd ./$1
+
+        suffix="$(echo $1 | head -c 1)"
+        local heroku_app_name=$HEROKU_APP_NAME-$suffix
+
+        echo "Creating app '$heroku_app_name'..."
+        heroku create -a $heroku_app_name --region eu || true
+
+        echo "Building image '$heroku_app_name:$BITBUCKET_COMMIT' and pushing to Heroku..."
+        docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+
+        echo "Building and pushing container..."
+        ID=$(docker build . -q -t $heroku_app_name:$BITBUCKET_COMMIT)
+        NAME="registry.heroku.com/$heroku_app_name/web"
+        docker tag $ID $NAME
+        docker push $NAME
+        
+        echo "Releasing container to '$heroku_app_name'..."
+        heroku container:release web -a $heroku_app_name
+
+        echo "Deployed app to https://$heroku_app_name.herokuapp.com"
+
+        echo "cd to root..."
+        cd ../
+    fi
+}
+
 HEROKU_APP_NAME=$(prep_heroku_app_name "$PROJECT_NAME-$BITBUCKET_BRANCH")
 
 install_lint_test_and_build module true
 install_lint_test_and_build server false
 
-docker_build_and_push client &
+docker_build_and_push client
 docker_build_and_push server
