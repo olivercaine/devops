3feef8d0b2c0c7f0f0f92643fd01184fe29c8f31
Oliver Caine <olliecaine@gmail.com>
2019-05-26 23:25:23 +1000

    Tools

--- .bash
+++ .bash
@@ -4,20 +4,6 @@ source ~/.nvm/nvm.sh
 
 alias bash_project='code $(dirname $0)/.functions'
 
-function install_tools {
-    echo "Installing Brew..."
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-    echo "Homebrew Cask..."
-    brew tap caskroom/cask
-
-    echo "Installing Docker packages..."
-    brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
-
-    echo "Installing API and database apps..."
-    brew cask install docker
-}
-
 function seed {
     git submodule add -b master https://github.com/olivercaine/common.git common
     git submodule add -b master https://github.com/olivercaine/devops.git devops
