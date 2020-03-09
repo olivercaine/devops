97a90ef1e48eb9591befb1af9d8bc12ee870c591
Oliver Caine <olliecaine@gmail.com>
2020-03-09 17:42:58 +1100

    Adding merge_to_all_branches_from function

--- .bash
+++ .bash
@@ -6,7 +6,7 @@ CLIENT=https://github.com/olivercaine/react-redux-starter-kit-extended.git
 MODULE=https://github.com/olivercaine/typescript-library-boilerplate.git
 SERVER=https://github.com/olivercaine/express-typescript-boilerplate.git
 
-function seed {
+seed () {
     git submodule add -b master https://github.com/olivercaine/common.git common
     git submodule add -b master https://github.com/olivercaine/devops.git devops
     git subtree add --prefix=client $CLIENT modpack/latest --squash
@@ -14,23 +14,36 @@ function seed {
     git subtree add --prefix=server $SERVER modpack/latest --squash
 }
 
-function update_client {
+update_client () {
     git subtree pull --prefix=client $CLIENT modpack/latest --squash
 }
 
-function update_module {
+update_module () {
     git subtree pull --prefix=module $MODULE modpack/latest --squash
 }
 
-function update_server {
+update_server () {
     git subtree pull --prefix=server $SERVER modpack/latest --squash
 }
 
-function update_modules {
+update_modules () {
     cd client/devops;gco master;gl;../common;gco master;gl;../../devops;gco master;gl;../;
 }
 
-function uat {
+uat () {
     local directory=${PWD##*/}
     open "https://${directory}-${3:-$(git symbolic-ref -q --short HEAD)}-c.herokuapp.com/"
+}
+
+merge_to_all_branches_from () {
+    local merge_from=${1:-$(git symbolic-ref --short HEAD)} # Merge to all branches from current branch by default
+
+    for branch in $(git for-each-ref --format="%(refname:short)" refs/heads); do
+    if [[ "${branch}" != "master" ]]; then
+        echo "Merge from $merge_from to ${branch}"
+        git checkout "${branch}"
+        git merge $merge_from
+        git push --no-verify
+    fi
+    done
 }
